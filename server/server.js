a75c4bbb409835e8fed234a40da0cbcd7dee7556
ktoto2007 <shagako2007@gmail.com>
2026-06-27 15:53:07 +0300

    connected db with server and added get and pull requests

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const pool = require("./db");
 const express = require("express");
 const cors = require("cors");
 
@@ -6,8 +7,43 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Backend работает" });
+pool.query("SELECT NOW()", (err, result) => {
+  if (err) {
+      console.error("Ошибка подключения к БД:", err.message);
+  } else {
+      console.log("База данных подключена!");
+      console.log(result.rows[0]);
+  }
+});
+
+app.get("/todos", async (req, res) => {
+  try {
+      const result = await pool.query(
+          "SELECT * FROM todos ORDER BY id"
+      );
+      res.json(result.rows);
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({
+          error: "Ошибка сервера"
+      });
+  }
+});
+
+app.post("/todos", async (req, res) => {
+  try {
+      const { title } = req.body;
+      const result = await pool.query(
+          "INSERT INTO todos (title) VALUES ($1) RETURNING *",
+          [title]
+      );
+      res.status(201).json(result.rows[0]);
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({
+          error: "Ошибка сервера"
+      });
+  }
 });
 
 app.listen(5000, () => {
