9e4090dae35854716123e67aca9e7bf772ff3a7d
ktoto2007 <shagako2007@gmail.com>
2026-06-30 13:15:03 +0300

    change in vite.config.js

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    allowedHosts: ["client-production-5a68.up.railway.app"],
+  },
 })
